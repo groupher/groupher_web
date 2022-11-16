dcbadd8ed08b9c6b6472342f8b805e47574a0baa
mydearxym <mydearxym@qq.com>
2022-11-16 17:23:15 +0800

    chore: update deps

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 
 // next-plugins
 // const withPlugins = require('next-compose-plugins')
-// const withPWA = require('next-pwa')
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
