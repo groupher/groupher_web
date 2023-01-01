d3a0598c0bd3c00bc17e481da7b92e577fc80d45
mydearxym <mydearxym@qq.com>
2023-01-01 22:05:54 +0800

    refactor(feedback-footer): add with lastUpdate concept

---
+++ src/widgets/FeedbackFooter/BottomInfo.tsx
@@ -0,0 +1,59 @@
+/*
+ *
+ * FeedbackFooter
+ *
+ */
+
+import { FC, memo, useState } from 'react'
+
+import type { TSpace } from '@/spec'
+import { buildLog } from '@/utils/logger'
+
+import type { THelpFeedback } from './spec'
+import { HELP_FEEDBACK } from './constant'
+import {
+  Wrapper,
+  LastUpdateWrapper,
+  FeedbackWrapper,
+  Title,
+  FaceWraper,
+  IconWrapper,
+  GoodIcon,
+  SoSoIcon,
+  BadIcon,
+} from './styles/bottom_info'
+
+/* eslint-disable-next-line */
+const log = buildLog('c:FeedbackFooter:index')
+
+type TProps = {
+  offsetRight?: number
+  withLastUpdated?: boolean
+} & TSpace
+
+const FeedbackFooter: FC<TProps> = ({ offsetRight = 30, withLastUpdated = true }) => {
+  const { GOOD, BAD, SOSO } = HELP_FEEDBACK
+  const [feedback, setFeedback] = useState<THelpFeedback | ''>('')
+
+  return (
+    <Wrapper offsetRight={offsetRight} withLastUpdated={withLastUpdated}>
+      {withLastUpdated && <LastUpdateWrapper>最后更新: 3 天前</LastUpdateWrapper>}
+      <FeedbackWrapper withLastUpdated={withLastUpdated}>
+        <Title small={withLastUpdated}>本文是否有帮助?</Title>
+        <FaceWraper small={withLastUpdated}>
+          <IconWrapper $active={feedback === BAD} onClick={() => setFeedback(BAD)}>
+            <BadIcon $active={feedback === BAD} small={withLastUpdated} />
+          </IconWrapper>
+          <IconWrapper $active={feedback === SOSO} onClick={() => setFeedback(SOSO)}>
+            <SoSoIcon $active={feedback === SOSO} small={withLastUpdated} />
+          </IconWrapper>
+          <IconWrapper $active={feedback === GOOD} onClick={() => setFeedback(GOOD)}>
+            <GoodIcon $active={feedback === GOOD} small={withLastUpdated} />
+          </IconWrapper>
+        </FaceWraper>
+      </FeedbackWrapper>
+    </Wrapper>
+  )
+}
+
+export default memo(FeedbackFooter)
