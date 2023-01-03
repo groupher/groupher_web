14cebd72f1369232f22c06c45149aa044e614240
mydearxym <mydearxym@qq.com>
2023-01-03 09:49:32 +0800

    chore(help-thread): add cover demo

---
+++ src/containers/thread/HelpThread/styles/article_cover.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+// import css, { theme } from '@/utils/css'
+import Img from '@/Img'
+
+export const Wrapper = styled.div`
+  margin-top: 15px;
+`
+export const Image = styled(Img)`
+  margin-bottom: 26px;
+  width: 100%;
+  object-fit: cover;
+`
