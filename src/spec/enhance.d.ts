735c8e8748c0fa93d45219b050bd3631d84483d5
mydearxym <mydearxym@qq.com>
2023-01-03 21:07:38 +0800

    chore: clean up

--- src/spec/enhance.d.ts
+++ src/spec/enhance.d.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-unresolved */
 // see: https://github.com/sindresorhus/type-fest#template-literal-types
 
-export type { ScreamingSnakeCase as TSnakeUpperCase, LowerCase } from 'type-fest'
+export type { ScreamingSnakeCase as TSnakeUpperCase } from 'type-fest'
 
 export type {
   NegativeInteger as TNegativeInteger,
