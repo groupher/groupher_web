5f9d1f2a460a2473b7810477bfbd11f634108e1a
mydearxym <mydearxym@qq.com>
2023-01-06 22:19:48 +0800

    refactor(footer): fix link & bump version

--- src/widgets/LandingPage/Header.tsx
+++ src/widgets/LandingPage/Header.tsx
@@ -23,7 +23,7 @@ const Header: FC = () => {
         <LinkItem href={`/${ROUTE.HOME}/${ROUTE.KANBAN}`}>看板</LinkItem>
         <LinkItem href={`/${ROUTE.HOME}/${ROUTE.CHANGELOG}`}>更新日志</LinkItem>
         <LinkItem href={`/${ROUTE.HOME}/${ROUTE.HELP}`}>帮助台</LinkItem>
-        <LinkItem href={ROUTE.HOME}>讨论区</LinkItem>
+        <LinkItem href={`/${ROUTE.HOME}/${ROUTE.ABOUT}`}>关于</LinkItem>
       </LinksWrapper>
 
       <RequestDemo>
