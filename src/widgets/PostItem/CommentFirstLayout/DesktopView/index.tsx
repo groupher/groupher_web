bdbe04981db29f32f95df63931e749e14e3d3738
mydearxym <mydearxym@qq.com>
2023-02-18 21:33:36 +0800

    refactor(post-layout): adjust comment-fisrt desktop layout

--- src/widgets/PostItem/CommentFirstLayout/DesktopView/index.tsx
+++ src/widgets/PostItem/CommentFirstLayout/DesktopView/index.tsx
@@ -1,10 +1,9 @@
 import { FC, memo } from 'react'
 
-import type { TCommunity, TPost } from '@/spec'
-import { UPVOTE_LAYOUT } from '@/constant/layout'
+import type { TAvatarLayout, TCommunity, TPost } from '@/spec'
+import { AVATAR_LAYOUT, UPVOTE_LAYOUT } from '@/constant/layout'
 
 import { upvoteOnArticleList } from '@/utils/signal'
-import TheAvatar from '@/widgets/TheAvatar'
 import ArticleReadLabel from '@/widgets/ArticleReadLabel'
 import ArticlePinLabel from '@/widgets/ArticlePinLabel'
 import Upvote from '@/widgets/Upvote'
@@ -14,22 +13,18 @@ import ViewingSign from '../../ViewingSign'
 import Header from './Header'
 import Body from './Body'
 
-import {
-  Wrapper,
-  AvatarWrapper,
-  UpvoteWrapper,
-  Main,
-} from '../../styles/comment_fist_layout/desktop_view'
+import { Wrapper, Avatar, AvatarWrapper, Main } from '../../styles/comment_fist_layout/desktop_view'
 
 type TProps = {
   curCommunity: TCommunity | null
   article: TPost
+  avatarLayout?: TAvatarLayout
 
   // onUserSelect?: (obj: TUser) => void
   // onAuthorSelect?: (obj: TAccount) => void
 }
 
-const DigestView: FC<TProps> = ({ curCommunity, article }) => {
+const DigestView: FC<TProps> = ({ curCommunity, article, avatarLayout = AVATAR_LAYOUT.SQUARE }) => {
   return (
     <Wrapper>
       <ArticleReadLabel article={article} />
@@ -37,15 +32,13 @@ const DigestView: FC<TProps> = ({ curCommunity, article }) => {
 
       <ViewingSign article={article} top={12} />
       <AvatarWrapper>
-        <TheAvatar user={article.author} />
-        <UpvoteWrapper>
-          <Upvote
-            type={UPVOTE_LAYOUT.POST_LIST}
-            count={article.upvotesCount}
-            viewerHasUpvoted={article.viewerHasUpvoted}
-            onAction={(viewerHasUpvoted) => upvoteOnArticleList(article, viewerHasUpvoted)}
-          />
-        </UpvoteWrapper>
+        <Avatar src={article.author.avatar} avatarLayout={avatarLayout} />
+        <Upvote
+          type={UPVOTE_LAYOUT.POST_LIST}
+          count={article.upvotesCount}
+          viewerHasUpvoted={article.viewerHasUpvoted}
+          onAction={(viewerHasUpvoted) => upvoteOnArticleList(article, viewerHasUpvoted)}
+        />
       </AvatarWrapper>
       <Main>
         <Header article={article} />
--- src/widgets/PostItem/styles/comment_fist_layout/desktop_view/index.ts
+++ src/widgets/PostItem/styles/comment_fist_layout/desktop_view/index.ts
@@ -1,6 +1,9 @@
 import styled from 'styled-components'
 
-import css, { theme } from '@/utils/css'
+import type { TAvatarLayout } from '@/spec'
+import { AVATAR_LAYOUT } from '@/constant/layout'
+
+import css from '@/utils/css'
 import Img from '@/Img'
 
 export const Wrapper = styled.div`
@@ -12,18 +15,12 @@ export const Main = styled.div`
   ${css.flexColumnGrow()};
 `
 export const AvatarWrapper = styled.div`
-  ${css.flexColumn('align-both')};
-  margin-top: -3px;
+  ${css.flexColumn('align-center', 'justify-between')};
+  padding-top: 10px;
+  padding-bottom: 2px;
+  margin-right: 8px;
 `
-export const UpvoteWrapper = styled.div`
-  margin-left: -4px;
-`
-export const Avatar = styled(Img)`
-  ${css.circle(36)};
-  fill: ${theme('article.title')};
-  opacity: ${theme('avatar.opacity')};
-  margin-top: 2px;
-`
-export const SmallAvatar = styled(Avatar)`
-  ${css.size(35)};
+export const Avatar = styled(Img)<{ avatarLayout: TAvatarLayout }>`
+  ${css.size(26)};
+  border-radius: ${({ avatarLayout }) => (avatarLayout === AVATAR_LAYOUT.SQUARE ? '6px' : '100%')};
 `
