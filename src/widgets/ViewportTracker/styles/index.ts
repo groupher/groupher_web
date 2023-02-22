1235cc1a897040cb0a42fe441d97705d2657e816
mydearxym <mydearxym@qq.com>
2023-02-22 23:51:47 +0800

    chore: debug crash

--- src/widgets/ViewportTracker/styles/index.ts
+++ src/widgets/ViewportTracker/styles/index.ts
@@ -1,11 +1,7 @@
 import styled from 'styled-components'
 
-import type { TTestable } from '@/spec'
-
 // import Img from '@/Img'
 
-export const Wrapper = styled.div.attrs(({ testid }: TTestable) => ({
-  'data-test-id': testid,
-}))<TTestable>``
+export const Wrapper = styled.div``
 
 export const Title = styled.div``
