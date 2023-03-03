5491754e5349ec29664684a6ce498dfca527ec35
mydearxym <mydearxym@qq.com>
2023-03-03 12:24:51 +0800

    feat(pwa): try basic pwa config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@
 // next-plugins
 // const withPlugins = require('next-compose-plugins')
 
+const withPWA = require('next-pwa')({
+  dest: 'public',
+})
+
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
@@ -26,4 +30,4 @@ const nextConfig = {
   },
 }
 
-module.exports = withBundleAnalyzer(nextConfig)
+module.exports = withBundleAnalyzer(withPWA(nextConfig))
