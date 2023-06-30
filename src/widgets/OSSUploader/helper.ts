b68b93574d9119c7762808c42a3967eeba96d85e
mydearxym <mydearxym@qq.com>
2023-06-30 10:10:47 +0800

    refactor: update pkg

--- src/widgets/OSSUploader/helper.ts
+++ src/widgets/OSSUploader/helper.ts
@@ -12,14 +12,10 @@ const getOSSDir = (): string => {
 
 export const initOSSClient = (): any => {
   const ossClient = new OSS.Wrapper({
-    // region: process.env.NEXT_PUBLIC_ALI_OSS_RESION,
-    // accessKeyId: process.env.NEXT_PUBLIC_ALI_ACCESS_KEY,
-    // accessKeySecret: process.env.NEXT_PUBLIC_ALI_ACCESS_SECRET,
-    // bucket: process.env.NEXT_PUBLIC_ALI_OSS_BUCKET,
-    region: 'oss-cn-shanghai',
-    accessKeyId: 'LTAI5tGbMRig5pytvsYCfkLK',
-    accessKeySecret: 'acnQofqG56fGnJ4DhmEjXAJXR9hlkg',
-    bucket: 'cps-oss',
+    region: process.env.NEXT_PUBLIC_ALI_OSS_RESION,
+    accessKeyId: process.env.NEXT_PUBLIC_ALI_ACCESS_KEY,
+    accessKeySecret: process.env.NEXT_PUBLIC_ALI_ACCESS_SECRET,
+    bucket: process.env.NEXT_PUBLIC_ALI_OSS_BUCKET,
     /* internal: true, */
     /* secure: true, */
   })
