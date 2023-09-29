74ec91975ce5c52563492c70b1e795905471fe52
mydearxym <mydearxym@qq.com>
2023-09-29 08:43:08 +0800

    refactor(theme): wip

--- src/containers/layout/GlobalLayout/Wallpaper.tsx
+++ src/containers/layout/GlobalLayout/Wallpaper.tsx
@@ -2,20 +2,22 @@ import { FC } from 'react'
 import { observer } from 'mobx-react'
 
 import useWallpaper from '@/hooks/useWallpaper'
+import useTheme from '@/hooks/useTheme'
+
 import { parseWallpaper } from '@/wallpaper'
 
 import { Wrapper } from './styles/wallpaper'
 
 const Wallpaper: FC = () => {
-  const theWallpaper = useWallpaper()
+  const { wallpapers, wallpaper, customWallpaper } = useWallpaper()
+  const themeName = useTheme()
 
-  const { wallpapers, wallpaper, customWallpaper } = theWallpaper
   const { background, effect } = parseWallpaper(wallpapers, wallpaper, customWallpaper)
 
   // for custom image/svg
   // for use style object not passing props
   // @link see https://github.com/styled-components/styled-components/issues/3315#issuecomment-885977691
-  return <Wrapper style={{ background }} effect={effect} />
+  return <Wrapper style={{ background }} effect={effect} themeName={themeName} />
 }
 
 export default observer(Wallpaper)
