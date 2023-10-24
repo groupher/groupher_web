22ce938c1070b98506f2a4abd2b5ab4f45463806
mydearxym <mydearxym@qq.com>
2023-10-24 19:36:08 +0800

    chore: wip

---
+++ src/widgets/Article/styles/changelog/content.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+
+import css from '@/css'
+
+export const Wrapper = styled.div`
+  ${css.row('justify-center')};
+  position: relative;
+  min-height: 250px;
+  width: 100%;
+
+  ${css.media.mobile`
+    padding: 0 20px;
+    margin-left: 0;
+  `};
+`
+export const InnerWrapper = styled.div`
+  width: 100%;
+`
+export const ArticleWrapper = styled.div`
+  font-size: 15px;
+  min-height: 200px;
+  max-width: 580px;
+`
+export const BodyHeaderWrapper = styled.div`
+  margin-top: -18px;
+  margin-bottom: 18px;
+`
+export const CommentsWrapper = styled.div`
+  margin-top: 35px;
+`
---
+++ src/widgets/Article/styles/changelog/index.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+import type { TTestable, TMetric } from '@/spec'
+import css from '@/css'
+
+type TWrapper = { metric: TMetric } & TTestable
+export const Wrapper = styled.nav.attrs<TWrapper>(({ testid }) => ({
+  'data-test-id': testid,
+}))<TWrapper>`
+  ${css.column('align-center')};
+  position: relative;
+  background: transparent;
+  min-height: 248px;
+  height: auto;
+  margin-bottom: 24px;
+  width: 100%;
+  ${({ metric }) => css.fitPageWidth(metric)};
+`
+export const HeaderWrapper = styled.div`
+  margin-left: -16px;
+`
+export const InnerWrapper = styled.div<{ metric: TMetric }>`
+  width: 100%;
+  ${({ metric }) => css.fitContentWidth(metric)};
+`
+export const BannerContent = styled.div`
+  ${css.row('justify-start')};
+`
+export const Main = styled.div`
+  padding-right: 30px;
+  flex-grow: 1;
+`
