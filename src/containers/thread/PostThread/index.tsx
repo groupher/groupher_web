2abb0116ca514ca6946d22f68efd5c2e9eadda38
mydearxym <mydearxym@qq.com>
2023-11-24 20:08:06 +0800

    style(post-content): clean up

--- src/containers/thread/PostThread/index.tsx
+++ src/containers/thread/PostThread/index.tsx
@@ -4,7 +4,7 @@
  *
  */
 
-import { FC, useEffect, useRef } from 'react'
+import { FC, useRef } from 'react'
 import { observer } from 'mobx-react-lite'
 
 import type { TResState } from '@/spec'
@@ -18,27 +18,15 @@ import { THREAD } from '@/constant/thread'
 
 import PagedPosts from '@/widgets/PagedPosts'
 import TagNote from '@/widgets/TagNote'
-import ViewportTracker from '@/widgets/ViewportTracker'
 import ArticlesFilter from '@/widgets/ArticlesFilter'
 // import LavaLampLoading from '@/widgets/Loading/LavaLampLoading'
 
 import ThreadSidebar from './ThreadSidebar'
 
 import { Wrapper, MainWrapper, SidebarWrapper, FilterWrapper } from './styles'
-import { inAnchor, outAnchor, onFilterSelect } from './logic'
 
 /* eslint-disable-next-line */
 const log = buildLog('C:PostThread')
-
-const isInViewport = (element) => {
-  const rect = element.getBoundingClientRect()
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  )
-}
 
 const PostThread: FC = () => {
   const bannerLayout = useBannerLayout()
@@ -46,12 +34,6 @@ const PostThread: FC = () => {
 
   const postLayout = usePostLayout()
   const trackerRef = useRef(null)
-
-  useEffect(() => {
-    if (trackerRef.current) {
-      isInViewport(trackerRef.current) ? inAnchor() : outAnchor()
-    }
-  }, [trackerRef])
 
   // if (store.curThread !== THREAD.POST) return <LavaLampLoading top={20} />
 
@@ -64,8 +46,6 @@ const PostThread: FC = () => {
   return (
     <Wrapper>
       <LayoutWrapper $thread={THREAD.POST} $postLayout={postLayout}>
-        <ViewportTracker onEnter={inAnchor} onLeave={outAnchor} />
-
         {showFilters && (
           <FilterWrapper ref={trackerRef}>
             <ArticlesFilter isMobile={isMobile} resState={resState as TResState} mode="default" />
