9073f928dffaf1f73c634c506b9700ad3a29953b
xieyimian <mydearxym@qq.com>
2023-11-30 20:10:11 +0800

    refactor(dashboard): layout wrapper style & init logic (#188)

--- src/app/[community]/dashboard/styles/index.ts
+++ src/app/[community]/dashboard/styles/index.ts
@@ -1,28 +1,20 @@
 import styled from 'styled-components'
 
-import type { TMetric, TTestable } from '@/spec'
+import type { TMetric } from '@/spec'
 import css from '@/css'
 
-export const BaseWrapper = styled.div.attrs<TTestable>(({ $testid }) => ({
-  'data-test-id': $testid,
-}))<TTestable>`
+export const Wrapper = styled.div<{ metric?: TMetric }>`
+  ${css.column('justify-start', 'align-center')};
   min-height: 70vh;
   width: 100%;
 
-  ${css.media.tablet`
+  ${css.media.mobile`
+    padding-left: 0;
     width: 100%;
     margin: 0;
     padding: .6em;
     padding-top: 0;
     padding-right: 0;
-  `};
-`
-
-export const Wrapper = styled(BaseWrapper)<{ metric?: TMetric }>`
-  ${css.column('justify-start', 'align-center')};
-
-  ${css.media.mobile`
-    padding-left: 0;
   `};
 `
 
@@ -34,8 +26,28 @@ export const InnerWrapper = styled.div<{ metric: TMetric }>`
   padding-top: 0;
   ${css.column('align-center')};
 `
-
 export const ContentWrapper = styled.div`
   ${css.column()};
   width: 100%;
+`
+export const FrameWrapper = styled.div<{ metric: TMetric }>`
+  ${css.row()};
+  width: 100%;
+  ${({ metric }) => css.fitPageWidth(metric)};
+`
+export const MainWrapper = styled.div`
+  flex-grow: 1;
+  min-height: 500px;
+
+  background: transparent;
+  margin-top: 30px;
+  padding-left: 80px;
+  margin-left: 35px;
+
+  ${css.media.mobile`
+    margin-top: 20px;
+    padding-left: 0;
+    margin-left: 0;
+    width: 100%;
+  `};
 `
