591991a86e6f95f4422b072e30913dd76bb68b11
mydearxym <mydearxym@qq.com>
2024-01-07 11:05:36 +0800

    feat(landing): SEO wip

--- src/app/Landing/DashboardIntros/SeoTab/ContentCard.tsx
+++ src/app/Landing/DashboardIntros/SeoTab/ContentCard.tsx
@@ -1,13 +1,14 @@
 import { FC } from 'react'
 import { useParallax } from 'react-scroll-parallax'
 
-import { Wrapper } from '../../styles/dashboard_intros/seo_tab/content_card'
+import { Wrapper, WebCard } from '../../styles/dashboard_intros/seo_tab/content_card'
 
 const ContentCard: FC = () => {
   const { ref } = useParallax<HTMLDivElement>({ speed: 3 })
 
   return (
     <Wrapper ref={ref}>
+      <WebCard />
       <div>seo tab </div>
     </Wrapper>
   )
--- src/app/Landing/styles/dashboard_intros/seo_tab/content_card.ts
+++ src/app/Landing/styles/dashboard_intros/seo_tab/content_card.ts
@@ -3,17 +3,29 @@ import styled, { css, theme } from '@/css'
 export const Wrapper = styled.div`
   ${css.column()};
   padding: 20px;
+  background: #f1f1f154;
+  color: ${theme('article.digest')};
+  width: 460px;
+  height: 540px;
+  border-radius: 15px;
+  z-index: 2;
+  position: absolute;
+  bottom: 80px;
+  left: 140px;
+`
+export const WebCard = styled.div`
+  ${css.column()};
+  padding: 20px;
   background: ${theme('htmlBg')};
   color: ${theme('article.digest')};
   border: 1px solid;
   border-color: ${theme('divider')};
-  width: 420px;
-  height: 460px;
+  width: 430px;
+  height: 160px;
   border-radius: 15px;
   box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
   z-index: 2;
   position: absolute;
-  bottom: 160px;
-  left: 140px;
+  top: 12px;
+  left: 12px;
 `
-export const holder = 1
