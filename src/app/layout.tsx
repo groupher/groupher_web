737559e3a199dba939d2b167c45477e595af4518
mydearxym <mydearxym@qq.com>
2024-02-29 20:44:38 +0800

    chore(vercel): add basic seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from 'next'
 import { cookies } from 'next/headers'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
@@ -8,6 +9,11 @@ import {
   RootStoreProvider,
   GlobalLayout,
 } from './providers'
+
+export const metadata: Metadata = {
+  title: 'Groupher | 让你的产品听见用户的声音',
+  description: '讨论区、看板、更新日志、帮助文档多合一，收集沉淀用户反馈，助你打造更好的产品。',
+}
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const token = cookies().get('jwtToken')?.value || null
