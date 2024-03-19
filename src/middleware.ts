d6c404f257d8c981eb0b51ee1ed3297946d413c8
mydearxym <mydearxym@qq.com>
2024-03-19 18:58:13 +0800

    refactor(middleware): wip

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,37 +1,19 @@
-import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
+import { applyMiddleware } from './middlewares/helper'
 // import { themeMiddleware } from './middlewares/theme'
 import { queryWhitelistMiddleware } from './middlewares/query-whitelist'
 import { oopsMiddleware } from './middlewares/oops'
 import { avoidScanMiddleware } from './middlewares/avoid-scan'
 
-function applyMiddleware(middlewareFns, req) {
-  for (const middlewareFn of middlewareFns) {
-    const response = middlewareFn(req)
-    // 如果中间件执行结果是重定向或重写，那么就返回该响应并且不再执行后续中间件
-
-    if (response instanceof NextResponse) {
-      // 检查状态码是否代表重定向
-      if (response.status >= 300 && response.status < 400) {
-        return response
-      }
-    }
-  }
-
-  // 如果没有中间件返回重定向的NextResponse，就继续处理请求
-  return NextResponse.next()
-}
-
 export function middleware(request: NextRequest) {
-  // 这里我们创建一个数组，按期望的执行顺序包含所有的中间件函数
+  // middleware in this array will be applied in order
   const middlewareFunctions = [
     avoidScanMiddleware,
     oopsMiddleware,
     queryWhitelistMiddleware,
-    // ... 添加更多中间件函数
+    // ... more middlewares
   ]
 
-  // 使用applyMiddleware函数来执行中间件
   return applyMiddleware(middlewareFunctions, request)
 }
