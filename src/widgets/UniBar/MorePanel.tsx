d97e4a345f27cec85f61a14e4b8a6a3ee41f2d58
mydearxym <mydearxym@qq.com>
2024-05-02 16:59:46 +0800

    chore: link to dashbaord

--- src/widgets/UniBar/MorePanel.tsx
+++ src/widgets/UniBar/MorePanel.tsx
@@ -1,26 +1,33 @@
 import { FC } from 'react'
 import { observer } from 'mobx-react-lite'
 
-import useCommunityDigestViewport from '@/hooks/useCommunityDigestViewport'
 import useTrans from '@/hooks/useTrans'
+import useCommunityDigestViewport from '@/hooks/useCommunityDigestViewport'
+import useViewingCommunity from '@/hooks/useViewingCommunity'
+
+import { DASHBOARD_ROUTE } from '@/constant/route'
 
 import HomeLogo from '@/widgets/HomeLogo'
-import { SpaceGrow, SexyDivider } from '@/widgets/Common'
+import { SpaceGrow, SexyDivider, LinkAble } from '@/widgets/Common'
 
 import { Wrapper, MenuBar, Icon } from './styles/more_panel'
 
 const MorePanel: FC = () => {
   const { inView: badgeInView } = useCommunityDigestViewport()
+  const { slug } = useViewingCommunity()
   const { t } = useTrans()
 
   return (
     <Wrapper>
-      <MenuBar $withTop={!badgeInView}>
-        <Icon.Dashboard />
-        {t('dashboard')}
-        <SpaceGrow />
-        <Icon.Link />
-      </MenuBar>
+      <LinkAble href={`/${slug}/${DASHBOARD_ROUTE.OVERVIEW}`}>
+        <MenuBar $withTop={!badgeInView}>
+          <Icon.Dashboard />
+          {t('dashboard')}
+          <SpaceGrow />
+          <Icon.Link />
+        </MenuBar>
+      </LinkAble>
+
       <SexyDivider top={6} bottom={6} />
       <MenuBar $withTop={!badgeInView}>
         <Icon.Report />
