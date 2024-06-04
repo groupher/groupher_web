b6a6fd61f98cf630e8ef7cce19c88bb68c7cbfe9
mydearxym <mydearxym@qq.com>
2024-06-04 12:08:04 +0800

    chore: links wip

--- src/containers/thread/DashboardThread/logic/useLinks/index.ts
+++ src/containers/thread/DashboardThread/logic/useLinks/index.ts
@@ -1,2 +1,290 @@
 // logics for header & footer links
+import { keys, find, findIndex, clone, remove, filter, reject, forEach } from 'ramda'
 
+import { runInAction, toJS } from '@/mobx'
+
+import { ONE_LINK_GROUP, MORE_GROUP } from '@/const/dashboard'
+import { CHANGE_MODE } from '@/const/mode'
+
+import type { TLinkItem, TGroupedLinks } from '@/spec'
+import useDashboard from '@/hooks/useDashboard'
+
+import type { TMoveLinkDir } from '../../spec'
+import { EMPTY_LINK_ITEM } from '../../constant'
+
+import useCommon from './useCommon'
+
+type TRet = {
+  resetEditingLink: () => void
+  updateEditingGroup: (title: string) => void
+  updateInGroup: (link: TLinkItem) => void
+  add2Group: (group: string, index: number) => void
+  deleteLink: (linkItem: TLinkItem) => void
+  deleteGroup: (groupIndex: number) => void
+  cancelLinkEditing: () => void
+  confirmLinkEditing: () => void
+  updateEditingLink: (key: string, value: string) => void
+  //
+
+  triggerGroupUpdate: (title: string, index: number) => void
+  cancelGroupChange: () => void
+  triggerGroupAdd: () => void
+  //
+  addHeaderLinkGroup: () => void
+  moveAboutLink2Bottom: () => void
+  moveLink: (link: TLinkItem, dir: TMoveLinkDir) => void
+  //
+  moveGroup2Left: (group: string) => void
+  moveGroup2Right: (group: string) => void
+  moveGroup2EdgeLeft: (group: string) => void
+  moveGroup2EdgeRight: (group: string) => void
+}
+
+const useLinks = (): TRet => {
+  const { dashboard: store } = useDashboard()
+  const {
+    moveGroup,
+    reindex,
+    doMoveLink,
+    doMoveLink2Edge,
+    reindexGroup,
+    emptyLinksIfNedd,
+    confirmGroupAdd,
+    keepMoreGroup2EndIfNeed,
+  } = useCommon()
+
+  const updateInGroup = (link: TLinkItem): void => {
+    store.editingLink = link
+    store.editingLinkMode = CHANGE_MODE.UPDATE
+  }
+
+  const add2Group = (group: string, index: number): void => {
+    const { curPageLinksKey } = store
+    const links = store[curPageLinksKey.settings][curPageLinksKey.links]
+
+    const grouplinks = filter((link: TLinkItem) => link.group === group, links)
+
+    if (grouplinks.length <= 0) return
+    const { groupIndex } = grouplinks[0]
+
+    const newItem = {
+      ...EMPTY_LINK_ITEM,
+      index,
+      group,
+      groupIndex,
+    }
+
+    runInAction(() => {
+      store[curPageLinksKey.links] = [...links, newItem]
+      store.editingLink = newItem
+      store.editingLinkMode = CHANGE_MODE.CREATE
+    })
+  }
+
+  const deleteLink = (linkItem: TLinkItem): void => {
+    const { curPageLinksKey } = store
+    const links = store[curPageLinksKey.settings][curPageLinksKey.links]
+
+    let linksAfter = reject(
+      (link: TLinkItem) => link.group === linkItem.group && link.index === linkItem.index,
+      links,
+    )
+
+    linksAfter = emptyLinksIfNedd(linksAfter)
+
+    store[curPageLinksKey.links] = reindex(linksAfter)
+    // store.mark({ [curPageLinksKey.links]: _reindex(linksAfter) })
+  }
+
+  const deleteGroup = (groupIndex: number): void => {
+    const { curPageLinksKey } = store
+    const links = store[curPageLinksKey.settings][curPageLinksKey.links]
+
+    let linksAfter = reject((link: TLinkItem) => link.groupIndex === groupIndex, links)
+
+    linksAfter = emptyLinksIfNedd(linksAfter)
+
+    store[curPageLinksKey.links] = reindexGroup(linksAfter)
+  }
+
+  const cancelLinkEditing = (): void => {
+    const { curPageLinksKey, editingLink, editingLinkMode, initSettings } = store
+    const links = store[curPageLinksKey.settings][curPageLinksKey.links]
+
+    if (editingLinkMode === CHANGE_MODE.UPDATE) {
+      store.editingLink = null
+      return
+    }
+
+    let linksAfter = reject(
+      (link: TLinkItem) => link.group === editingLink.group && link.index === editingLink.index,
+      links,
+    )
+
+    linksAfter = emptyLinksIfNedd(linksAfter)
+
+    runInAction(() => {
+      store[curPageLinksKey.links] = linksAfter
+      store.editingLink = null
+      store.initSettings = { ...toJS(initSettings), [curPageLinksKey.links]: linksAfter }
+    })
+  }
+
+  const confirmLinkEditing = (): void => {
+    const { curPageLinksKey, editingLink, editingLinkMode } = store
+    const links = store[curPageLinksKey.settings][curPageLinksKey.links]
+
+    if (editingLinkMode === CHANGE_MODE.UPDATE) {
+      const editingIndex = findIndex(
+        (item: TLinkItem) => item.index === editingLink.index && item.group === editingLink.group,
+        links,
+      )
+
+      links[editingIndex].title = editingLink.title
+      links[editingIndex].link = editingLink.link
+
+      store.mark({ [curPageLinksKey.links]: links, editingLink: null })
+      return
+    }
+
+    const curGroupLinks = filter((link: TLinkItem) => editingLink.group === link.group, links)
+
+    const newAddLink = find(
+      (link: TLinkItem) =>
+        editingLink.group === link.group && link.index === curGroupLinks.length - 1,
+      links,
+    )
+
+    const editingLinkAfter = {
+      ...editingLink,
+      index: newAddLink.index,
+      group: newAddLink.group,
+      groupIndex: newAddLink.groupIndex,
+    }
+
+    const linksAfter = reject(
+      (link: TLinkItem) => link.group === newAddLink.group && link.index === newAddLink.index,
+      links,
+    ).concat(editingLinkAfter)
+
+    store[curPageLinksKey.links] = linksAfter
+    store.editingLink = null
+
+    keepMoreGroup2EndIfNeed()
+
+    if (newAddLink.group === MORE_GROUP) {
+      moveAboutLink2Bottom()
+    }
+  }
+
+  const updateEditingLink = (key: string, value: string): void => {
+    const { editingLink } = store
+
+    const editingLinkAfter = { ...editingLink, [key]: value }
+
+    store.editingLink = editingLinkAfter
+  }
+
+  const resetEditingLink = (): void => {
+    runInAction(() => {
+      store.editingLink = null
+      store.editingGroup = null
+      store.editingGroupIndex = null
+    })
+  }
+
+  const updateEditingGroup = (title: string): void => {
+    store.editingGroup = title
+  }
+
+  const triggerGroupUpdate = (title: string, index: number): void => {
+    runInAction(() => {
+      store.editingGroup = title
+      store.editingGroupIndex = index
+    })
+  }
+
+  const triggerGroupAdd = (): void => {
+    runInAction(() => {
+      store.editingGroup = ''
+      store.editingGroupIndex = null
+    })
+  }
+
+  const cancelGroupChange = (): void => {
+    runInAction(() => {
+      store.editingGroup = null
+      store.editingGroupIndex = null
+    })
+  }
+
+  const addHeaderLinkGroup = () => {
+    const time = new Date().getTime()
+
+    store.editingGroup = `${ONE_LINK_GROUP}_${time}`
+    confirmGroupAdd()
+  }
+
+  const moveLink = (link: TLinkItem, dir: TMoveLinkDir): void => {
+    switch (dir) {
+      case 'up':
+        doMoveLink(link, 'up')
+        return
+      case 'down':
+        doMoveLink(link, 'down')
+        return
+      case 'top':
+        doMoveLink2Edge(link, 'top')
+        return
+      case 'bottom':
+        doMoveLink2Edge(link, 'bottom')
+        return
+
+      default:
+    }
+  }
+
+  const moveAboutLink2Bottom = (): void => {
+    const { curPageLinksKey, headerSettings } = store
+    if (curPageLinksKey.links !== 'headerLinks') return
+
+    const aboutLink = find(
+      (item) => item.group === MORE_GROUP && item.title === '关于',
+      headerSettings.headerLinks,
+    )
+
+    moveLink(aboutLink, 'bottom')
+  }
+
+  const moveGroup2Left = (group: string): void => moveGroup(group, 'left')
+  const moveGroup2Right = (group: string): void => moveGroup(group, 'right')
+
+  const moveGroup2EdgeLeft = (group: string): void => moveGroup(group, 'edge-left')
+  const moveGroup2EdgeRight = (group: string): void => moveGroup(group, 'edge-right')
+
+  return {
+    updateInGroup,
+    add2Group,
+    deleteLink,
+    deleteGroup,
+    cancelLinkEditing,
+    updateEditingLink,
+    confirmLinkEditing,
+    resetEditingLink,
+    updateEditingGroup,
+    triggerGroupUpdate,
+    triggerGroupAdd,
+    cancelGroupChange,
+    //
+    addHeaderLinkGroup,
+    moveLink,
+    moveAboutLink2Bottom,
+    //
+    moveGroup2Left,
+    moveGroup2Right,
+    moveGroup2EdgeLeft,
+    moveGroup2EdgeRight,
+  }
+}
+
+export default useLinks
--- src/containers/thread/DashboardThread/logic/useLinks/useCommon.ts
+++ src/containers/thread/DashboardThread/logic/useLinks/useCommon.ts
@@ -1,17 +1,28 @@
 // logics for header & footer links
 
-import { keys, findIndex, clone, remove, filter, reject, forEach } from 'ramda'
+import { keys, findIndex, clone, remove, filter, reject, forEach, find } from 'ramda'
+
+import { runInAction } from '@/mobx'
+import { sortByIndex, groupByKey } from '@/helper'
 
 import type { TLinkItem, TGroupedLinks } from '@/spec'
+import { ROUTE } from '@/const/route'
+import { ONE_LINK_GROUP, MORE_GROUP } from '@/const/dashboard'
 
-import { sortByIndex, groupByKey } from '@/helper'
 import useDashboard from '@/hooks/useDashboard'
 
-type TRet = {
-  moveGroup2Left: (group: string) => void
-  moveGroup2Right: (group: string) => void
-  moveGroup2EdgeLeft: (group: string) => void
-  moveGroup2EdgeRight: (group: string) => void
+import { EMPTY_LINK_ITEM } from '../../constant'
+
+export type TRet = {
+  moveGroup: (group: string, opt: 'left' | 'right' | 'edge-left' | 'edge-right') => void
+  reindex: (links: TLinkItem[]) => TLinkItem[]
+  keepMoreGroup2EndIfNeed: () => void
+  doMoveLink: (link: TLinkItem, opt: 'up' | 'down') => void
+  doMoveLink2Edge: (link: TLinkItem, opt: 'top' | 'bottom') => void
+  reindexGroup: (_targetLinks: TLinkItem[]) => TLinkItem[]
+  emptyLinksIfNedd: (links: TLinkItem[]) => TLinkItem[]
+  confirmGroupAdd: () => void
+  confirmGroupUpdate: () => void
 }
 
 /**
@@ -19,6 +30,183 @@ type TRet = {
  */
 const useCommon = (): TRet => {
   const { dashboard: store } = useDashboard()
+
+  const emptyLinksIfNedd = (links: TLinkItem[]): TLinkItem[] => {
+    const { curPageLinksKey } = store
+
+    if (
+      curPageLinksKey.links === 'headerLinks' &&
+      links.length === 1 &&
+      links[0].group === MORE_GROUP
+    ) {
+      return []
+    }
+
+    return links
+  }
+
+  const reindex = (links: TLinkItem[]): TLinkItem[] => {
+    return links.map((item, index) => ({
+      ...item,
+      index,
+    }))
+  }
+
+  /**
+   * move links actions
+   */
+  const doMoveLink = (link: TLinkItem, opt: 'up' | 'down'): void => {
+    const { group } = link
+    const { curPageLinksKey } = store
+    const links = store[curPageLinksKey.settings][curPageLinksKey.links]
+
+    const groupLinks = filter((item: TLinkItem) => item.group === group, links)
+    const restLinks = reject((item: TLinkItem) => item.group === group, links)
+
+    const linkIndex = findIndex((item: TLinkItem) => item.index === link.index, groupLinks)
+
+    const targetIndex = opt === 'up' ? linkIndex - 1 : linkIndex + 1
+
+    const tmp = groupLinks[targetIndex]
+    groupLinks[targetIndex] = groupLinks[linkIndex]
+    groupLinks[linkIndex] = tmp
+
+    const tmpIndex = groupLinks[targetIndex].index
+    groupLinks[targetIndex].index = groupLinks[linkIndex].index
+    groupLinks[linkIndex].index = tmpIndex
+
+    store[curPageLinksKey.links] = [...restLinks, ...reindex(groupLinks)]
+    // store.mark({ [curPageLinksKey.links]: [...restLinks, ..._reindex(groupLinks)] })
+  }
+
+  const doMoveLink2Edge = (link: TLinkItem, opt: 'top' | 'bottom'): void => {
+    const { group } = link
+    const { curPageLinksKey } = store
+    const links = store[curPageLinksKey.settings][curPageLinksKey.links]
+
+    const groupLinks = filter((item: TLinkItem) => item.group === group, links)
+    const restLinks = reject((item: TLinkItem) => item.group === group, links)
+
+    const curLinkItemIndex = findIndex((item: TLinkItem) => item.index === link.index, groupLinks)
+    const curLinkItem = clone(groupLinks[curLinkItemIndex])
+
+    const newLinks =
+      opt === 'top'
+        ? [curLinkItem, ...remove(curLinkItemIndex, 1, groupLinks)]
+        : [...remove(curLinkItemIndex, 1, groupLinks), curLinkItem]
+
+    store[curPageLinksKey.links] = [...restLinks, ...reindex(newLinks)]
+    // store.mark({ [curPageLinksKey.links]: [...restLinks, ...reindex(newLinks)] })
+  }
+
+  /**
+   * move group actions
+   */
+  const reindexGroup = (_targetLinks: TLinkItem[]): TLinkItem[] => {
+    // @ts-ignore
+    const targetLinks = clone(sortByIndex(_targetLinks, 'groupIndex'))
+
+    const _groupedLinks = groupByKey(targetLinks, 'group')
+    const groupKeys = keys(_groupedLinks) as string[]
+
+    for (let i = 0; i < groupKeys.length; i += 1) {
+      const gkey = groupKeys[i]
+
+      for (let j = 0; j < targetLinks.length; j += 1) {
+        if (targetLinks[j].group === gkey) {
+          targetLinks[j].groupIndex = i
+        }
+      }
+    }
+
+    return targetLinks as TLinkItem[]
+  }
+
+  const keepMoreGroup2EndIfNeed = (): void => {
+    const { curPageLinksKey, curCommunity } = store
+    if (curPageLinksKey.links !== 'headerLinks') return
+
+    const links = store.headerSettings.headerLinks
+
+    const _groupedLinks = groupByKey(links, 'group')
+    const groupKeys = keys(_groupedLinks) as string[]
+
+    const moreGroup = find((item: string) => item === MORE_GROUP, groupKeys)
+
+    // create if no custom link exists
+    if (links.length > 0 && !moreGroup) {
+      const newLinkItem = {
+        ...EMPTY_LINK_ITEM,
+        title: '关于',
+        link: `/${curCommunity.slug}/${ROUTE.ABOUT}`,
+        group: MORE_GROUP,
+        // make sure the "more" gorup is always in the end
+        groupIndex: groupKeys.length + 2,
+      }
+
+      const linksAfter = [...links, newLinkItem]
+
+      store.headerLinks = reindexGroup(linksAfter)
+    } else {
+      // make sure the "more" gorup is always in the end
+      const linksAfter = links.map((item) => {
+        return {
+          ...item,
+          groupIndex: item.group === MORE_GROUP ? links.length + 2 : item.groupIndex,
+        }
+      })
+
+      store.headerLinks = reindexGroup(linksAfter)
+    }
+  }
+
+  const confirmGroupAdd = (): void => {
+    const { curPageLinksKey, editingGroup } = store
+    const links = store[curPageLinksKey.settings][curPageLinksKey.links]
+
+    const _groupedLinks = groupByKey(links, 'group')
+    const groupKeys = keys(_groupedLinks) as string[]
+
+    const newLinkItem = {
+      ...EMPTY_LINK_ITEM,
+      group: editingGroup,
+      groupIndex: groupKeys.length,
+    }
+
+    const linksAfter = [...links, newLinkItem]
+
+    runInAction(() => {
+      store.editingGroup = null
+      store.editingLink = newLinkItem
+      store[curPageLinksKey.links] = linksAfter
+    })
+
+    keepMoreGroup2EndIfNeed()
+  }
+
+  const confirmGroupUpdate = (): void => {
+    const { curPageLinksKey, editingGroup, editingGroupIndex } = store
+    const links = store[curPageLinksKey.settings][curPageLinksKey.links]
+
+    // store.mark({ [curPageLinksKey.links]: _reindexGroup(links) })
+
+    // const linksAfter = clone(_reindexGroup(links))
+    const linksAfter = clone(links)
+
+    for (let i = 0; i < links.length; i += 1) {
+      const linkItem = links[i]
+
+      if (linkItem.groupIndex === editingGroupIndex) {
+        linksAfter[i].group = editingGroup
+      }
+    }
+
+    runInAction(() => {
+      store.editingGroup = null
+      store.editingGroupIndex = null
+      store[curPageLinksKey.links] = linksAfter
+    })
+  }
 
   const _reindexByGroup = (groupKeys: string[], groupedLinks: TGroupedLinks): TGroupedLinks => {
     for (let index = 0; index < groupKeys.length; index += 1) {
@@ -33,7 +221,7 @@ const useCommon = (): TRet => {
     return groupedLinks
   }
 
-  const _moveGroup = (group: string, opt: 'left' | 'right' | 'edge-left' | 'edge-right'): void => {
+  const moveGroup = (group: string, opt: 'left' | 'right' | 'edge-left' | 'edge-right'): void => {
     const { curPageLinksKey } = store
     const links = store[curPageLinksKey.settings][curPageLinksKey.links]
 
@@ -85,17 +273,16 @@ const useCommon = (): TRet => {
     // store.mark({ [curPageLinksKey.links]: newLinks })
   }
 
-  const moveGroup2Left = (group: string): void => _moveGroup(group, 'left')
-  const moveGroup2Right = (group: string): void => _moveGroup(group, 'right')
-
-  const moveGroup2EdgeLeft = (group: string): void => _moveGroup(group, 'edge-left')
-  const moveGroup2EdgeRight = (group: string): void => _moveGroup(group, 'edge-right')
-
   return {
-    moveGroup2Left,
-    moveGroup2Right,
-    moveGroup2EdgeLeft,
-    moveGroup2EdgeRight,
+    moveGroup,
+    reindex,
+    keepMoreGroup2EndIfNeed,
+    doMoveLink,
+    doMoveLink2Edge,
+    reindexGroup,
+    emptyLinksIfNedd,
+    confirmGroupAdd,
+    confirmGroupUpdate,
   }
 }
 
