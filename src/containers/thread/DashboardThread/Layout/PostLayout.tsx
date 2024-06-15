ae18aa3d1c55b5a53523fc1c6e40f612a9f7dc89
mydearxym <mydearxym@qq.com>
2024-06-15 17:38:53 +0800

    refactor: dashboard hooks -> logic

--- src/containers/thread/DashboardThread/Layout/PostLayout.tsx
+++ src/containers/thread/DashboardThread/Layout/PostLayout.tsx
@@ -1,6 +1,3 @@
-import type { FC } from 'react'
-import { observer } from 'mobx-react-lite'
-
 import { POST_LAYOUT, DASHBOARD_DESC_LAYOUT } from '@/const/layout'
 import usePrimaryColor from '@/hooks/usePrimaryColor'
 import { callDashboardDesc } from '@/signal'
@@ -13,7 +10,7 @@ import { SETTING_FIELD } from '../constant'
 import SectionLabel from '../SectionLabel'
 import SavingBar from '../SavingBar'
 
-import usePostInfo from '../hooks/usePostInfo'
+import usePost from '../logic/usePost'
 import {
   Wrapper,
   SelectWrapper,
@@ -29,11 +26,12 @@ import {
   UpvoteIcon,
   CommentIcon,
 } from '../styles/layout/post_layout'
-import { edit } from '../logic'
 
-const PostListLayout: FC = () => {
-  const { layout, isTouched, saving } = usePostInfo()
+export default () => {
+  const { layout, getIsTouched, saving, edit } = usePost()
   const primaryColor = usePrimaryColor()
+
+  const isTouched = getIsTouched()
 
   return (
     <Wrapper>
@@ -212,5 +210,3 @@ const PostListLayout: FC = () => {
     </Wrapper>
   )
 }
-
-export default observer(PostListLayout)
