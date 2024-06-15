daae8b34dea083a9dea256dd2374dff6a08ddf74
mydearxym <mydearxym@qq.com>
2024-06-15 15:11:00 +0800

    refactor: general useLayout

--- src/hooks/useAvatarLayout.ts
+++
@@ -1,8 +0,0 @@
-import type { TAvatarLayout } from '@/spec'
-import useSubStore from '@/hooks/useSubStore'
-
-export default (): TAvatarLayout => {
-  const store = useSubStore('dashboard')
-
-  return store.avatarLayout
-}
