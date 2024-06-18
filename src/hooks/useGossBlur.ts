c91c9b9155c905f2cf6c063f0a93f28298f5c204
mydearxym <mydearxym@qq.com>
2024-06-18 21:38:43 +0800

    fix: gossblur hook

--- src/hooks/useGossBlur.ts
+++ src/hooks/useGossBlur.ts
@@ -1,24 +1,13 @@
-import { useContext } from 'react'
-import { MobXProviderContext } from 'mobx-react'
-
 import useTheme from '@/hooks/useTheme'
 import THEME from '@/const/theme'
 
-/**
- * NOTE: should use observer to wrap the component who use this hook
- */
-const useGossBlur = (): number => {
+import useSubstore from '@/hooks/useSubStore'
+
+export default (): number => {
+  const store = useSubstore('dashboard')
   const { theme } = useTheme()
-  const { store } = useContext(MobXProviderContext)
 
-  if (store === null) {
-    throw new Error('Store cannot be null, please add a context provider')
-  }
-
-  const { dashboardThread } = store
-  const { gossBlur, gossBlurDark } = dashboardThread
+  const { gossBlur, gossBlurDark } = store
 
   return theme === THEME.DAY ? gossBlur : gossBlurDark
 }
-
-export default useGossBlur
