257281765b9d3587962aaadbe571323b51ec2339
mydearxym <mydearxym@qq.com>
2024-07-10 18:45:09 +0800

    chore: basic setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from 'next'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 
+import '../globals.css'
+
 import {
   StyledComponentsRegistry,
   GraphQLProvider,
