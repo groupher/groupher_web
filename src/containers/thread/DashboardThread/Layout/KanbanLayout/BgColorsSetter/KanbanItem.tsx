93441ba378ce40dede9f85a53b79eb57df0a5f47
mydearxym <mydearxym@qq.com>
2024-09-26 10:12:18 +0800

    chore: wip

--- src/containers/thread/DashboardThread/Layout/KanbanLayout/BgColorsSetter/KanbanItem.tsx
+++ src/containers/thread/DashboardThread/Layout/KanbanLayout/BgColorsSetter/KanbanItem.tsx
@@ -1,31 +1,25 @@
 import type { FC } from 'react'
 
-import {
-  Wrapper,
-  Bar,
-  Footer,
-  UpvoteIcon,
-  Count,
-} from '../../../styles/layout/kanban_layout/bg_colors_setter/kanben_item'
+import UpvoteSVG from '~/icons/Upvote'
+
+import useSalon, { cn } from '../../../styles/layout/kanban_layout/bg_colors_setter/kanben_item'
 
 type TProps = {
-  opacity?: number
-  count?: number
-  width?: number
+  opacity?: string
+  width?: string
 }
 
-const KanbanItem: FC<TProps> = ({ opacity = 1, count = 9, width = 80 }) => {
-  return (
-    <Wrapper opacity={opacity}>
-      <Bar top={6} height={3} width={20} opacity={0.5} />
-      <Bar top={5} height={4} width={width} bottom={6} opacity={0.8} />
+const KanbanItem: FC<TProps> = ({ opacity = 'opacity-100', width = 'w-20' }) => {
+  const s = useSalon()
 
-      <Footer>
-        <UpvoteIcon />
-        <Count>{count}</Count>
-        <Bar height={3} width={14} top={3} opacity={0.3} />
-      </Footer>
-    </Wrapper>
+  return (
+    <div className={cn(s.wrapper, opacity)}>
+      <div className={cn(s.bar, 'top-2 w-14 mt-0.5 opacity-20')} />
+      <div className={cn(s.bar, 'top-5 h-1.5 opacity-30', width)} />
+
+      <UpvoteSVG className={s.icon} />
+      <div className={cn(s.bar, 'bottom-2 left-6 h-1 w-6 opacity-10')} />
+    </div>
   )
 }
 
