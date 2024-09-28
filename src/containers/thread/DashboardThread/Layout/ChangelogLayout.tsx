60835cfaf171894175fb1d91c0c38226d8cd423a
mydearxym <mydearxym@qq.com>
2024-09-28 16:59:04 +0800

    refactor: changelog done

--- src/containers/thread/DashboardThread/Layout/ChangelogLayout.tsx
+++ src/containers/thread/DashboardThread/Layout/ChangelogLayout.tsx
@@ -1,8 +1,6 @@
 import { CHANGELOG_LAYOUT, DASHBOARD_DESC_LAYOUT } from '~/const/layout'
-import usePrimaryColor from '~/hooks/usePrimaryColor'
 import { callDashboardDesc } from '~/signal'
 
-import { Row, Br, Space, Divider, Inline } from '~/widgets/Common'
 import ArrowButton from '~/widgets/Buttons/ArrowButton'
 import CheckLabel from '~/widgets/CheckLabel'
 
@@ -11,190 +9,80 @@ import SectionLabel from '../SectionLabel'
 import SavingBar from '../SavingBar'
 
 import useChangelog from '../logic/useChangelog'
-import {
-  Wrapper,
-  SelectWrapper,
-  Layout,
-  LayoutTitle,
-  Block,
-  Bar,
-  MiniBar,
-  MiniItem,
-  MiniIntro,
-  Column,
-  UpvoteIcon,
-  Picture,
-} from '../styles/layout/changelog_layout'
+import useSalon, { cn } from '../styles/layout/changelog_layout'
 
 export default () => {
+  const s = useSalon()
   const { edit, layout, getIsTouched, saving } = useChangelog()
-  const primaryColor = usePrimaryColor()
   const isTouched = getIsTouched()
 
   return (
-    <Wrapper>
+    <div className={s.wrapper}>
       <SectionLabel
         title="更新日志布局"
         desc={
           <>
             「更新日志」列表的默认展现形式，切换布局不影响已发布内容。
-            <Inline>
+            <div className="inline-flex">
               <ArrowButton
                 onClick={() => callDashboardDesc(DASHBOARD_DESC_LAYOUT.POST_LIST)}
                 fontSize={12}
               >
                 查看示例
               </ArrowButton>
-            </Inline>
+            </div>
           </>
         }
       />
-      <SelectWrapper>
-        <Layout onClick={() => edit(CHANGELOG_LAYOUT.CLASSIC, 'changelogLayout')}>
-          <Block $active={layout === CHANGELOG_LAYOUT.CLASSIC} $color={primaryColor}>
-            <Column>
-              <Picture />
-              <Br top={14} />
-              <Row>
-                <Bar long={60} />
-                <Space right={10} />
-                <Bar thin long={10} />
-              </Row>
-              <Br bottom={15} />
-              <Bar long={80} thin />
-              <Br top={10} />
-              <Bar long={30} thin />
-              <Br top={10} />
-              <Row>
-                <UpvoteIcon size={12} />
-                <Space right={5} />
-                <Bar long={12} thin />
-              </Row>
-            </Column>
-            <Divider />
-            <Column>
-              <Picture />
-              <Br top={14} />
-              <Row>
-                <Bar long={60} />
-                <Space right={10} />
-                <Bar thin long={10} />
-              </Row>
-              <Br bottom={15} />
-              <Bar long={80} thin />
-              <Br top={10} />
-              <Bar long={30} thin />
-              <Br top={10} />
-              <Row>
-                <UpvoteIcon size={12} />
-                <Space right={5} />
-                <Bar long={12} thin />
-              </Row>
-            </Column>
-          </Block>
-          <LayoutTitle $active={layout === CHANGELOG_LAYOUT.CLASSIC}>
-            <CheckLabel title="经典模式" active={layout === CHANGELOG_LAYOUT.CLASSIC} top={4} />
-          </LayoutTitle>
-        </Layout>
-        <Layout onClick={() => edit(CHANGELOG_LAYOUT.SIMPLE, 'changelogLayout')}>
-          <Block $active={layout === CHANGELOG_LAYOUT.SIMPLE} $color={primaryColor}>
-            <MiniItem>
-              <MiniBar long={10} thin />
-              <MiniIntro>
-                <Row>
-                  <MiniBar long={46} />
-                  <Space right={5} />
-                  <MiniBar thin long={8} />
-                  <div className="grow" />
-                </Row>
-                <Br top={8} />
-                <MiniBar thin long={90} />
-                <Br top={8} />
-                <MiniBar thin long={80} />
-                <Br top={8} />
-                <MiniBar thin long={40} />
+      <div className={s.select}>
+        <div className={s.layout} onClick={() => edit(CHANGELOG_LAYOUT.CLASSIC, 'changelogLayout')}>
+          <div className={cn(s.block, layout === CHANGELOG_LAYOUT.CLASSIC && s.blockActive)}>
+            <div className={cn(s.cover, 'left-16')} />
+            <div className={cn(s.bar, 'h-2.5 top-28 left-16 ml-0.5 opacity-30')} />
+            <div className={cn(s.bar, 'h-1.5 top-32 left-16 ml-0.5 mt-2 w-32 opacity-20')} />
+            <div className={cn(s.bar, 'h-1.5 top-36 left-16 ml-0.5 mt-2 w-28 opacity-10')} />
 
-                <Br top={8} />
-                <MiniBar long={20} />
-              </MiniIntro>
-            </MiniItem>
-            <Br top={24} />
-            <MiniItem>
-              <MiniBar long={10} thin />
-              <MiniIntro>
-                <Row>
-                  <MiniBar long={46} />
-                  <Space right={5} />
-                  <MiniBar thin long={8} />
-                  <div className="grow" />
-                </Row>
-                <Br top={8} />
-                <MiniBar thin long={90} />
-                <Br top={8} />
-                <MiniBar thin long={80} />
-                <Br top={8} />
-                <MiniBar thin long={40} />
+            <div className={cn(s.cover, 'left-16 bottom-16')} />
+            <div className={cn(s.bar, 'h-2.5 w-14 bottom-10 left-16 ml-0.5 opacity-30')} />
+            <div className={cn(s.bar, 'h-1.5 bottom-6 left-16 ml-0.5 mt-2 w-28 opacity-15')} />
+          </div>
 
-                <Br top={8} />
-                <MiniBar long={20} />
-              </MiniIntro>
-            </MiniItem>
-            <Br top={24} />
-            <MiniItem>
-              <MiniBar long={10} thin />
-              <MiniIntro>
-                <Row>
-                  <MiniBar long={46} />
-                  <Space right={5} />
-                  <MiniBar thin long={8} />
-                  <div className="grow" />
-                </Row>
-                <Br top={8} />
-                <MiniBar thin long={90} />
-                <Br top={8} />
-                <MiniBar thin long={80} />
-                <Br top={8} />
-                <MiniBar thin long={40} />
+          <CheckLabel title="经典模式" active={layout === CHANGELOG_LAYOUT.CLASSIC} top={4} />
+        </div>
+        <div className={s.layout} onClick={() => edit(CHANGELOG_LAYOUT.SIMPLE, 'changelogLayout')}>
+          <div className={cn(s.block, layout !== CHANGELOG_LAYOUT.SIMPLE && s.blockActive)}>
+            <div className={cn(s.bar, 'h-1.5 w-7 top-5 mt-0.5 left-10 ml-0.5 opacity-20')} />
+            <div className={cn(s.bar, 'h-2.5 top-5 left-24 ml-0.5 opacity-30')} />
+            <div className={cn(s.bar, 'h-1.5 top-10 left-24 ml-0.5 mt-2 w-28 opacity-30')} />
+            <div className={cn(s.bar, 'h-1.5 top-14 left-24 ml-0.5 mt-2 w-24 opacity-20')} />
 
-                <Br top={8} />
-                <MiniBar long={20} />
-              </MiniIntro>
-            </MiniItem>
-            <Br top={24} />
-            <MiniItem>
-              <MiniBar long={10} thin />
-              <MiniIntro>
-                <Row>
-                  <MiniBar long={46} />
-                  <Space right={5} />
-                  <MiniBar thin long={8} />
-                  <div className="grow" />
-                </Row>
-                <Br top={8} />
-                <MiniBar thin long={90} />
-                <Br top={8} />
-                <MiniBar thin long={80} />
-                <Br top={8} />
-                <MiniBar thin long={40} />
+            <div className={cn(s.thumbnil, 'top-20 left-24')} />
+            <div className={cn(s.thumbnil, 'top-20 left-36 ml-2')} />
 
-                <Br top={8} />
-                <MiniBar long={20} />
-              </MiniIntro>
-            </MiniItem>
-          </Block>
+            <div className={cn(s.bar, 'h-1.5 w-7 top-36 mt-0.5 left-10 ml-0.5 opacity-20')} />
+            <div className={cn(s.bar, 'h-2.5 w-16 top-36 left-24 ml-0.5 opacity-30')} />
+            <div className={cn(s.bar, 'h-1.5 top-40 left-24 ml-0.5 mt-2 w-24 opacity-30')} />
+            <div className={cn(s.bar, 'h-1.5 top-44 left-24 ml-0.5 mt-2 w-28 opacity-20')} />
+            <div className={cn(s.bar, 'h-1.5 top-48 left-24 ml-0.5 mt-2 w-16 opacity-10')} />
 
-          <LayoutTitle $active={layout === CHANGELOG_LAYOUT.SIMPLE}>
-            <CheckLabel title="极简模式" active={layout === CHANGELOG_LAYOUT.SIMPLE} top={4} />
-          </LayoutTitle>
-        </Layout>
-      </SelectWrapper>
+            <div className={cn(s.thumbnil, 'top-52 mt-2.5 left-24')} />
+            <div className={cn(s.thumbnil, 'top-52 mt-2.5 left-36 ml-2')} />
+
+            <div className={cn(s.bar, 'h-1.5 w-8 bottom-6 mt-0.5 left-10 ml-0.5 opacity-20')} />
+            <div className={cn(s.bar, 'h-2.5 w-16 bottom-6 left-24 ml-0.5 opacity-30')} />
+          </div>
+
+          <CheckLabel title="极简模式" active={layout === CHANGELOG_LAYOUT.SIMPLE} top={4} />
+        </div>
+      </div>
 
       <SavingBar
         isTouched={isTouched}
         field={SETTING_FIELD.CHANGELOG_LAYOUT}
         loading={saving}
-        top={20}
+        width="w-11/12"
+        top={12}
       />
-    </Wrapper>
+    </div>
   )
 }
