2e5c1f99f425cfb6f63ec7535e62f434e4ee9f7f
mydearxym <mydearxym@qq.com>
2024-10-03 17:01:18 +0800

    chore: remove styled in dashboard, wip

--- src/containers/thread/DashboardThread/styles/broadcast/templates/article/default.ts
+++ src/containers/thread/DashboardThread/styles/broadcast/templates/article/default.ts
@@ -1,12 +1,17 @@
-import styled, { css, theme } from '~/css'
+import useTwBelt from '~/hooks/useTwBelt'
 
-import { TemplateBlock } from '.'
+import useBase from '../..'
 
-export const Wrapper = styled(TemplateBlock)`
-  ${css.row('align-both')};
-  height: 180px !important;
-  background: ${({ $active }) => ($active ? theme('alphaBg') : 'transparent')};
-`
-export const Shrink = styled.div`
-  transform: scale(0.9);
-`
+export { cn } from '~/css'
+
+export default () => {
+  const { cn, fill, bg } = useTwBelt()
+  const base = useBase()
+
+  return {
+    wrapper: cn(base.blockBase, 'w-full h-80 px-12 py-8'),
+    active: cn(base.blockBaseActive),
+    bar: cn('h-1 w-20 rounded opacity-40', bg('text.digest')),
+    icon: cn('size-3', fill('button.fg')),
+  }
+}
