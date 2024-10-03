85086bd50c827012de918015c2ab6f74c1115628
mydearxym <mydearxym@qq.com>
2024-10-03 17:43:46 +0800

    chore: remove styled in dashboard, wip

--- src/containers/thread/DashboardThread/Layout/GlowLight.tsx
+++ src/containers/thread/DashboardThread/Layout/GlowLight.tsx
@@ -1,8 +1,9 @@
 import { GLOW_OPACITY, GLOW_EFFECTS_KEYS } from '~/const/glow_effect'
-import usePrimaryColor from '~/hooks/usePrimaryColor'
-import useTheme from '~/hooks/useTheme'
 
 import Radio from '~/widgets/Switcher/Radio'
+
+import DLightSVG from '~/icons/DLight'
+import ClossSVG from '~/icons/CloseLight'
 
 import SectionLabel from '../SectionLabel'
 import SavingBar from '../SavingBar'
@@ -10,19 +11,11 @@ import SavingBar from '../SavingBar'
 import { SETTING_FIELD } from '../constant'
 
 import useGlowLight from '../logic/useGlowLight'
-import {
-  Wrapper,
-  Row,
-  Box,
-  NoBox,
-  ForbidIcon,
-  CloseIcon,
-  GrowBackground,
-  SettingsRow,
-  SettingTitle,
-} from '../styles/layout/glow_light'
+import useSalon, { cn } from '../styles/layout/glow_light'
 
 export default () => {
+  const s = useSalon()
+
   const {
     glowType,
     glowFixed,
@@ -33,44 +26,45 @@ export default () => {
     saving,
     edit,
   } = useGlowLight()
-  const primaryColor = usePrimaryColor()
-  const { theme } = useTheme()
 
   const isTouched = getIsTouched()
   const isGrowFixedTouched = getIsGrowFixedTouched()
   const isGrowOpacityTouched = getIsGrowOpacityTouched()
 
   return (
-    <Wrapper>
+    <div className={s.wrapper}>
       <SectionLabel
         title="页面辉光"
         desc="设置后每个页面的展示光晕（阅览页面除外），可配合壁纸风格搭配。"
         width="96%"
       />
 
-      <Row>
-        <NoBox $active={glowType === ''} onClick={() => edit('', 'glowType')} $color={primaryColor}>
-          <ForbidIcon />
-          <CloseIcon />
-        </NoBox>
+      <div className={s.row}>
+        <div
+          className={cn(s.block, 'align-both', glowType === '' && s.block)}
+          onClick={() => edit('', 'glowType')}
+        >
+          <div className="column-align-both">
+            <DLightSVG className={s.icon} />
+            <ClossSVG className={cn(s.icon, 'size-5 opacity-60')} />
+          </div>
+        </div>
 
         {GLOW_EFFECTS_KEYS.map((effect) => (
-          <Box
+          <div
             key={effect}
-            $active={effect === glowType}
-            $color={primaryColor}
+            className={cn(s.block, effect === glowType && s.blockActive)}
             onClick={() => edit(effect, 'glowType')}
           >
-            <GrowBackground glowPosition="absolute" glowType={effect} $theme={theme} />
-          </Box>
+            <div className={s.bgWrapper} style={{ background: `${s.bgStyle2(effect)}` }} />
+          </div>
         ))}
-      </Row>
+      </div>
 
       <SavingBar
         isTouched={isTouched}
         field={SETTING_FIELD.GLOW_TYPE}
         loading={saving}
-        left={1}
         top={10}
         width="w-11/12"
       />
@@ -83,8 +77,8 @@ export default () => {
         loading={saving}
         width="w-11/12"
       >
-        <SettingsRow>
-          <SettingTitle>滑动跟随:</SettingTitle>
+        <div className={s.settings}>
+          <h3 className={s.title}>滑动跟随:</h3>
           <Radio
             size="small"
             items={[
@@ -100,7 +94,7 @@ export default () => {
             activeKey={glowFixed}
             onChange={(item) => edit(item.key, 'glowFixed')}
           />
-        </SettingsRow>
+        </div>
       </SavingBar>
 
       <div className="mb-10" />
@@ -111,11 +105,10 @@ export default () => {
           field={SETTING_FIELD.GLOW_OPACITY}
           loading={saving}
           width="88%"
-          left={-10}
           top={-8}
         >
-          <SettingsRow>
-            <SettingTitle>辉光强度:</SettingTitle>
+          <div className={s.settings}>
+            <h3 className={s.title}>辉光强度:</h3>
             <Radio
               size="small"
               items={[
@@ -131,9 +124,9 @@ export default () => {
               activeKey={glowOpacity}
               onChange={(item) => edit(item.key, 'glowOpacity')}
             />
-          </SettingsRow>
+          </div>
         </SavingBar>
       )}
-    </Wrapper>
+    </div>
   )
 }
