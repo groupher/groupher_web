e685da6dd79bea00a4cb5f99b12f3ebd7135693e
mydearxym <mydearxym@qq.com>
2024-10-14 11:35:22 +0800

    fix: battery grides

--- src/app/Landing/BatteryBento/GridBlocks/index.tsx
+++ src/app/Landing/BatteryBento/GridBlocks/index.tsx
@@ -2,22 +2,28 @@ import { type FC, useState } from 'react'
 import { keys, includes } from 'ramda'
 
 import useInterval from '~/hooks/useInterval'
-import { SexyDivider } from '~/widgets/Common'
 
-import { BLOCKS, SHINE_DOTS } from './constant'
+import EmojiSVG from '~/icons/HeartDuo'
+import UserSVG from '~/icons/UserDuo'
+import APISVG from '~/icons/API'
+import CloudSVG from '~/icons/CloudDuo'
+import SearchSVG from '~/icons/SearchDuo'
+import FingerSVG from '~/icons/FingerPrintDuo'
+
+import TriangleSVG from '~/icons/TriangleDuo'
+import SquareSVG from '~/icons/SquareDuo'
+import CircleSVG from '~/icons/CircleDuo'
+import StarSVG from '~/icons/StarDuo'
+import DiamandSVG from '~/icons/DiamandDuo'
+import SqaureSrewSVG from '~/icons/SqaureSrewDuo'
+
+import { SHINE_DOTS } from './constant'
 import Slogan from './Slogan'
 
-import {
-  Wrapper,
-  InnerWrapper,
-  Block,
-  Title,
-  Desc,
-  Line,
-  Icon,
-} from '../../styles/battery_bento/grid_blocks'
+import useSalon, { cn } from '../../styles/battery_bento/grid_blocks'
 
 const GridBlocks: FC = () => {
+  const s = useSalon()
   const [shineIdx, setShineIdx] = useState(0)
 
   useInterval(() => {
@@ -26,32 +32,117 @@ const GridBlocks: FC = () => {
   }, 2000)
 
   return (
-    <Wrapper>
+    <div className={s.wrapper}>
       <Slogan />
-      <InnerWrapper>
-        <SexyDivider top={0} />
-        <Line left={332} top={0} />
-        <Line right={332} top={0} />
+      <div className={s.inner}>
+        <div className={cn(s.rowLine, 'top-0')} />
+        <div className={cn(s.line, 'left-80 ml-3')} />
+        <div className={cn(s.line, 'right-80 mr-8')} />
+        <div className={cn(s.rowLine, 'top-40 mt-3')} />
 
-        <Icon.Square left={325} top={-7} $active={includes('1', SHINE_DOTS[shineIdx])} />
-        <Icon.Circle right={325} top={-7} $active={includes('2', SHINE_DOTS[shineIdx])} />
-        <Icon.SqaureSrew left={325} top={164} $active={includes('3', SHINE_DOTS[shineIdx])} />
-        <Icon.Diamand right={325} top={164} $active={includes('4', SHINE_DOTS[shineIdx])} />
-        <Icon.Star right={325} bottom={-6} $active={includes('5', SHINE_DOTS[shineIdx])} />
-        <Icon.Triangle left={325} bottom={-6} $active={includes('6', SHINE_DOTS[shineIdx])} />
+        <SquareSVG
+          className={cn(
+            s.vibeIcon,
+            'left-80 ml-1 -mt-1.5',
+            includes('1', SHINE_DOTS[shineIdx]) && s.vibeDown,
+          )}
+        />
+        <CircleSVG
+          className={cn(
+            s.vibeIcon,
+            'right-80 mr-6 -mt-1.5',
+            includes('2', SHINE_DOTS[shineIdx]) && s.vibeDown,
+          )}
+        />
+        <SqaureSrewSVG
+          className={cn(
+            s.vibeIcon,
+            'left-80 top-40 ml-1 mt-1.5',
+            includes('3', SHINE_DOTS[shineIdx]) && s.vibeDown,
+          )}
+        />
 
-        {BLOCKS.map((block) => (
-          <Block key={block.key}>
-            {block.key === 4 && <SexyDivider />}
-            {block.icon}
-            <Title>{block.title}</Title>
-            <Desc>{block.desc}</Desc>
-          </Block>
-        ))}
+        <DiamandSVG
+          className={cn(
+            s.vibeIcon,
+            'right-80 top-40 mr-6 mt-1.5',
+            includes('4', SHINE_DOTS[shineIdx]) && s.vibeDown,
+          )}
+        />
 
-        <SexyDivider bottom={0} />
-      </InnerWrapper>
-    </Wrapper>
+        <TriangleSVG
+          className={cn(
+            s.vibeIcon,
+            'left-80 -bottom-2 ml-1 mb-0.5',
+            includes('5', SHINE_DOTS[shineIdx]) && s.vibeDown,
+          )}
+        />
+
+        <StarSVG
+          className={cn(
+            s.vibeIcon,
+            'right-80 -bottom-2 mr-6 mb-0.5',
+            includes('5', SHINE_DOTS[shineIdx]) && s.vibeDown,
+          )}
+        />
+
+        <div className={s.block}>
+          <div className={s.iconBox}>
+            <CloudSVG className={cn(s.blockIcon, s.fillPurple, 'opacity-65')} />
+          </div>
+          <h3 className={s.title}>灵活部署</h3>
+          <div className={s.desc}>支持托管，以及国内外各种云服务提供商，一键私有部署。</div>
+        </div>
+
+        <div className={s.block}>
+          <div className={s.iconBox}>
+            <SearchSVG className={cn(s.blockIcon, s.fillBlue)} />
+          </div>
+          <h3 className={s.title}>全文搜索</h3>
+          <div className={s.desc}>基于业界领先的 Meilisearch，功能强大 & 快捷的全文搜索体验。</div>
+        </div>
+
+        <div className={s.block}>
+          <div className={s.iconBox}>
+            <FingerSVG className={cn(s.blockIcon, s.fillGreen)} />
+          </div>
+          <h3 className={s.title}>OAuth</h3>
+          <div className={s.desc}>只需少量配置，即可支持国内外主流 Oauth 登录方式。</div>
+        </div>
+
+        <div className={s.block}>
+          <div className={s.iconBox}>
+            <UserSVG className={cn(s.blockIcon, s.fillOrange)} />
+          </div>
+          <h3 className={s.title}>消息通知</h3>
+          <div className={s.desc}>
+            通过站内，邮件，微信等渠道及时通知用户，支持 webhook 自定义。
+          </div>
+        </div>
+
+        <div className={s.block}>
+          <div className={s.iconBox}>
+            <EmojiSVG className={cn(s.blockIcon, s.fillRed)} />
+          </div>
+          <h3 className={s.title}>Emoji 反馈</h3>
+          <div className={s.desc}>
+            帖子、更新日志等内容以及所有评论都支持 Emoji 表情，增加用户互动乐趣。
+          </div>
+        </div>
+
+        <div className={s.block}>
+          <div className={s.iconBox}>
+            <APISVG className={cn(s.blockIcon, s.fillCyan, 'opacity-60')} />
+          </div>
+          <h3 className={s.title}>开发者友好</h3>
+          <div className={s.desc}>
+            完全开源，前后端分离架构，可基于平台 API 二次开发自定义需求。
+          </div>
+        </div>
+
+        <div className={cn(s.rowLine, 'bottom-0')} />
+      </div>
+    </div>
   )
 }
 
--- src/app/Landing/styles/battery_bento/grid_blocks/index.ts
+++ src/app/Landing/styles/battery_bento/grid_blocks/index.ts
@@ -1,110 +1,33 @@
-import styled, { css, theme } from '~/css'
-import type { TActive, TSpace } from '~/spec'
+import { COLOR_NAME } from '~/const/colors'
 
-import EmojiSVG from '~/icons/HeartDuo'
-import UserSVG from '~/icons/UserDuo'
-import LaptopSVG from '~/icons/LaptopDuo'
-import CloudSVG from '~/icons/CloudDuo'
-import FingerPrintSVG from '~/icons/FingerPrintDuo'
-import SearchSVG from '~/icons/SearchDuo'
-//
-import TriangleSVG from '~/icons/TriangleDuo'
-import SquareSVG from '~/icons/SquareDuo'
-import CircleSVG from '~/icons/CircleDuo'
-import StarSVG from '~/icons/StarDuo'
-import DiamandSVG from '~/icons/DiamandDuo'
-import SqaureSrewSVG from '~/icons/SqaureSrewDuo'
+import useTwBelt from '~/hooks/useTwBelt'
 
-import { WithPosition } from '~/widgets/Common'
+export { cn } from '~/css'
 
-export const Wrapper = styled.div`
-  ${css.column('align-both')};
-  width: 1020px;
-  height: auto;
-  margin-bottom: 60px;
-`
-export const InnerWrapper = styled.div`
-  ${css.rowWrap()};
-  width: 100%;
-  height: auto;
-  position: relative;
-`
-type TIcon = TSpace & TActive
-const commonIcon = (comp) => {
-  return styled(comp)<TIcon>`
-    ${css.size(15)};
-    fill: ${({ $active }) => ($active ? theme('article.digest') : '#d2d2d2')};
-    position: absolute;
-    ${({ top }) => (top !== undefined ? `top: ${top}px;` : '')};
-    ${({ left }) => (left !== undefined ? `left: ${left}px;` : '')};
-    ${({ bottom }) => (bottom !== undefined ? `bottom: ${bottom}px;` : '')};
-    ${({ right }) => (right !== undefined ? `right: ${right}px;` : '')};
+export default () => {
+  const { cn, fg, bg, fill, rainbow, sexyHBorder } = useTwBelt()
 
-    transform: ${({ $active }) => ($active ? 'rotate(360deg)' : '')};
-    transition: all .8s;
-  `
+  return {
+    wrapper: cn('column-align-both w-[1400px] h-auto mb-16 px-44'),
+    inner: 'row wrap w-full h-auto relative',
+    block: 'column w-1/3 h-48 px-10 py-5',
+    title: cn('text-base bold-sm mt-5 mb-1.5', fg('text.digest')),
+    desc: cn('text-sm', fg('text.digest')),
+    //
+    iconBox: 'align-both size-8',
+    blockIcon: cn('size-7 opacity-50'),
+
+    vibeIcon: cn('absolute size-4 rotate-180 trans-all-200	!duration-1000', fill('text.digest')),
+    vibeDown: '-rotate-90 opacity-30',
+
+    fillPurple: rainbow(COLOR_NAME.PURPLE, 'fill'),
+    fillRed: rainbow(COLOR_NAME.RED, 'fill'),
+    fillGreen: rainbow(COLOR_NAME.GREEN, 'fill'),
+    fillOrange: rainbow(COLOR_NAME.ORANGE, 'fill'),
+    fillBlue: rainbow(COLOR_NAME.BLUE, 'fill'),
+    fillCyan: rainbow(COLOR_NAME.CYAN, 'fill'),
+    //
+    line: cn('absolute top-0 h-full w-px', bg('divider')),
+    rowLine: cn('absolute', sexyHBorder(35)),
+  }
 }
-
-export const Icon = {
-  Triangle: commonIcon(TriangleSVG),
-  Square: commonIcon(SquareSVG),
-  Circle: commonIcon(CircleSVG),
-  Star: commonIcon(StarSVG),
-  Diamand: commonIcon(DiamandSVG),
-  SqaureSrew: commonIcon(SqaureSrewSVG),
-}
-export const Block = styled.div`
-  ${css.column()};
-  width: 33.3%;
-  height: 130px;
-  padding: 0 40px;
-  padding-top: 5px;
-`
-export const UserIcon = styled(UserSVG)`
-  fill: purple;
-  ${css.size(24)};
-  opacity: 0.3;
-`
-export const EmojiIcon = styled(EmojiSVG)`
-  fill: tomato;
-  ${css.size(24)};
-  opacity: 0.5;
-`
-export const LaptopIcon = styled(LaptopSVG)`
-  fill: #008eff;
-  ${css.size(24)};
-  opacity: 0.36;
-`
-export const CloudIcon = styled(CloudSVG)`
-  fill: green;
-  ${css.size(24)};
-  opacity: 0.3;
-`
-export const SearchIcon = styled(SearchSVG)`
-  fill: #2bd1d0;
-  ${css.size(24)};
-  opacity: 0.5;
-`
-export const FingerPrintIcon = styled(FingerPrintSVG)`
-  fill: orange;
-  ${css.size(24)};
-  opacity: 0.6;
-`
-export const Title = styled.div`
-  font-size: 15px;
-  color: ${theme('article.digest')};
-  font-weight: 500;
-  margin-top: 20px;
-  margin-bottom: 7px;
-`
-export const Desc = styled.div`
-  font-size: 14px;
-  line-height: 21px;
-  color: ${theme('article.digest')};
-  opacity: 0.9;
-`
-export const Line = styled(WithPosition)`
-  width: 1px;
-  height: 100%;
-  background: ${theme('divider')};
-`
