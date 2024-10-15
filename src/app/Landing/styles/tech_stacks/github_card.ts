d056a24c9e5589e5b2d4c5bf37114319646f6b21
mydearxym <mydearxym@qq.com>
2024-10-15 18:45:00 +0800

    style: open source adjust

--- src/app/Landing/styles/tech_stacks/github_card.ts
+++ src/app/Landing/styles/tech_stacks/github_card.ts
@@ -1,5 +1,5 @@
+import type { TColorName } from '~/spec'
 import useWallpaper from '~/hooks/useWallpaper'
-import { COLOR_NAME } from '~/const/colors'
 
 import { getGithubGradient } from '../metric'
 
@@ -16,12 +16,10 @@ export default () => {
     wrapper: cn(
       'column w-[340px] h-96 rounded-xl p-5 px-8 bg-htmlBg-dark z-30 rotate-2 border-2',
       shadow('xl'),
-      rainbow(COLOR_NAME.PURPLE, 'border'),
+      rainbow(wallpaper as TColorName, 'border'),
     ),
     backgroundStyle: { background: getGithubGradient(wallpaper) },
-    //
     topping: 'row-center mb-1.5',
-    //
     githubIcon: cn('size-14 absolute top-3 right-6 opacity-15', fill('text.digest')),
     githubTitle: cn('text-lg clip-text mt-1.5'),
     gradientTextStyle: base.textGradientStyle,
