99a7cf13fd7e39e3a1f72eac7faf85a22fb506e2
mydearxym <mydearxym@qq.com>
2024-10-22 17:09:31 +0800

    chore: adjust

--- src/app/Landing/styles/dashboard_intros/layout_tab/content_card.ts
+++ src/app/Landing/styles/dashboard_intros/layout_tab/content_card.ts
@@ -7,7 +7,6 @@ export default () => {
     wrapper: cn(
       'column p-5 w-[420px] h-[450px] rounded-xl z-20',
       'absolute top-12 left-2.5',
-      'animate-wiggle animate-infinite animate-duration-[15000ms]',
       fg('text.digest'),
       bg('htmlBg'),
       shadow('lg'),
