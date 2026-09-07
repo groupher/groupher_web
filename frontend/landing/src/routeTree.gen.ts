e9d650e5ab425718ca6e49dfeebf0d2948d1c79f
mydearxym <mydearxym@qq.com>
2026-09-07 09:40:07 +0800

    chore(fe): clean unused frontend scaffolding

--- frontend/landing/src/routeTree.gen.ts
+++ frontend/landing/src/routeTree.gen.ts
@@ -8,11 +8,11 @@
 // You should NOT make any changes in this file as it will be overwritten.
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
-import { Route as R404RouteImport } from './routes/404'
 import { Route as rootRouteImport } from './routes/__root'
+import { Route as IndexRouteImport } from './routes/index'
+import { Route as R404RouteImport } from './routes/404'
 import { Route as BookDemoRouteImport } from './routes/book-demo'
 import { Route as HealthRouteImport } from './routes/health'
-import { Route as IndexRouteImport } from './routes/index'
 import { Route as PricingRouteImport } from './routes/pricing'
 
 const IndexRoute = IndexRouteImport.update({
